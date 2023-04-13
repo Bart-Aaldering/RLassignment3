25ef69b8f07b121d7a7e9efb624bb7b5f007263e
Bart <ba.aaldering@gmail.com>
2023-04-13 15:00:58 +0200

    ja

--- MBRLAgents.py
+++ MBRLAgents.py
@@ -12,7 +12,6 @@ from queue import PriorityQueue
 from MBRLEnvironment import WindyGridworld
 
 class DynaAgent:
-
     def __init__(self, n_states, n_actions, learning_rate, gamma):
         self.n_states = n_states
         self.n_actions = n_actions
@@ -33,36 +32,56 @@ class DynaAgent:
         return np.argmax(self.Q_sa[state])
         
     def update(self, state, action, reward, done, next_state, n_planning_updates):
+        if done:
+            return
+        
         self.n[state][action][next_state] += 1 
         self.r[state][action][next_state] += reward
         
-        self.Q_sa[state,action] += self.learning_rate * (reward + self.gamma * max(self.Q_sa[next_state])-self.Q_sa[state][action])
+        self.Q_sa[state][action] += self.learning_rate * (reward + self.gamma * max(self.Q_sa[next_state])-self.Q_sa[state][action])
 
-        for k in n_planning_updates:
-            s = np.where(np.sum(self.n[state][action], axis=) == 0)[0]
+        for _ in range(n_planning_updates):
+            # Choose random state with n>0
+            state_sums = np.sum(self.n, axis=(1,2))
+            state = np.random.choice(np.arange(state_sums.size)[state_sums>0])
+
+            # Choose previously taken action for state s  
+            action_sums = np.sum(self.n[state], axis=1)
+            action = np.random.choice(np.arange(action_sums.size)[action_sums>0])
             
-        # Update q value
-        self.q[state][action] += alpha*(next_reward+df*max(self.q[next_state])-self.q[state][action])
+            # Choose next state
+            p_hat = self.n[state][action]/np.sum(self.n[state][action])
+            next_state = np.random.choice(np.arange(self.n_states), p=p_hat)
+
+            reward = self.r[state][action][next_state]/self.n[state][action][next_state]
+            
+            # Update q value
+            self.Q_sa[state][action] += self.learning_rate * (reward + self.gamma * max(self.Q_sa[next_state])-self.Q_sa[state][action])
     
 class PrioritizedSweepingAgent:
-
     def __init__(self, n_states, n_actions, learning_rate, gamma, max_queue_size=200, priority_cutoff=0.01):
         self.n_states = n_states
         self.n_actions = n_actions
         self.learning_rate = learning_rate
-        self.gamma = gamma
+        self.gamma = gamma # discount factor
         self.priority_cutoff = priority_cutoff
         self.queue = PriorityQueue()
         
         self.Q_sa = np.zeros((n_states,n_actions))
         # TO DO: Initialize count tables, and reward sum tables. 
+        self.n = np.zeros((n_states, n_actions, n_states))
+        self.r = np.zeros((n_states, n_actions, n_states))
 
-    def select_action(self, s, epsilon):
-        # TO DO: Add own code
-        a = np.random.randint(0,self.n_actions) # Replace this with correct action selection
-        return a
+    def select_action(self, state, epsilon):
+        # If a random number is within the explore rate we explore by choosing a random action
+        if np.random.uniform(0,1) <= epsilon:
+            return np.random.choice(range(self.n_actions))
         
-    def update(self,s,a,r,done,s_next,n_planning_updates):
+        # If we don't explore we choose the action with the highest reward 
+        # If there are multiple actions with the same reward we choose the first one
+        return np.argmax(self.Q_sa[state])
+        
+    def update(self,state,action,reward,done,next_state,n_planning_updates):
         # TO DO: Add own code
         
         # Helper code to work with the queue
@@ -70,15 +89,51 @@ class PrioritizedSweepingAgent:
         # self.queue.put((-p,(s,a))) 
         # Retrieve the top (s,a) from the queue
         # _,(s,a) = self.queue.get() # get the top (s,a) for the queue
-        pass
+        if done:
+            return
+        
+        self.n[state][action][next_state] += 1 
+        self.r[state][action][next_state] += reward
+        
+        self.Q_sa[state][action] += self.learning_rate * (reward + self.gamma * max(self.Q_sa[next_state])-self.Q_sa[state][action])
+
+        p = abs(reward + self.gamma * max(self.Q_sa[next_state])-self.Q_sa[state][action])
+        if p > self.priority_cutoff:
+            self.queue.put((-p,(state,action)))
+
+
+        for _ in range(n_planning_updates):
+            if self.queue.empty():
+                break
+            p,(state, action) = self.queue.get()
+
+             # Choose next state
+            p_hat = self.n[state][action]/np.sum(self.n[state][action])
+            next_state = np.random.choice(np.arange(self.n_states), p=p_hat)
             
+            reward = self.r[state][action][next_state]/self.n[state][action][next_state]
+
+            # Update q value
+            self.Q_sa[state][action] += self.learning_rate * (reward + self.gamma * max(self.Q_sa[next_state])-self.Q_sa[state][action])
+
+            # Loop over all state actions that may lead to state s
+            for previous_state in range(self.n_states):
+                for action in range(self.n_actions):
+                    if self.n[previous_state][action][state] > 0:
+
+                        # Calculate reward and priority value
+                        reward = self.r[previous_state][action][state]/self.n[previous_state][action][state]
+                        p = abs(reward + self.gamma * max(self.Q_sa[previous_state])-self.Q_sa[previous_state][action])
+                        if p > self.priority_cutoff:
+                            self.queue.put((-p,(previous_state,action)))
+
 def test():
 
     n_timesteps = 1000
     gamma = 0.99
 
     # Algorithm parameters
-    policy = 'dyna' # 'ps' 
+    policy = 'ps' # 'ps' or 'dyna' 
     epsilon = 0.1
     learning_rate = 0.5
     n_planning_updates = 5
@@ -100,12 +155,13 @@ def test():
     # Prepare for running
     s = env.reset()  
     continuous_mode = False
-    
+    cumulative_r = 0
     for t in range(n_timesteps):            
         # Select action, transition, update policy
         a = pi.select_action(s,epsilon)
         s_next,r,done = env.step(a)
-        pi.update(s=s,a=a,r=r,done=done,s_next=s_next,n_planning_updates=n_planning_updates)
+        cumulative_r +=r
+        pi.update(s, a, r, done, s_next,n_planning_updates=n_planning_updates)
         
         # Render environment
         if plot:
@@ -122,6 +178,7 @@ def test():
             s = env.reset()
         else:
             s = s_next
+    print(f"Cumulative reward {cumulative_r}")
             
     
 if __name__ == '__main__':
--- MBRLExperiment.py
+++ MBRLExperiment.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 Model-based Reinforcement Learning experiments
-Practical for course 'Reinforcement Learning',
+Practical for course "Reinforcement Learning",
 Bachelor AI, Leiden University, The Netherlands
 2021
 By Thomas Moerland
@@ -20,10 +20,42 @@ def run_repetitions(policy, n_repetitions, n_timesteps, smoothing_window, learni
     # Average the learning curves over repetitions, and then additionally smooth the curve
     # Be sure to turn environment rendering off! It heavily slows down your runtime
     
-    learning_curve = np.random.rand(n_timesteps) # TO DO: replace this with a true experiment!
+    cumulative_rewards = np.zeros(n_timesteps)
+    
+    for i in range(n_repetitions):
+        rewards = np.zeros(n_timesteps)
+
+        # Initialize environment and policy
+        env = WindyGridworld()
+        if policy == "Dyna":
+            pi = DynaAgent(env.n_states,env.n_actions,learning_rate,gamma) # Initialize Dyna policy
+        elif policy == "Prioritized Sweeping":    
+            pi = PrioritizedSweepingAgent(env.n_states,env.n_actions,learning_rate,gamma) # Initialize PS policy
+        else:
+            raise KeyError(f"Policy {policy} not implemented")
+        
+        # Prepare for running
+        s = env.reset()  
+        total_r = 0
+        for t in range(n_timesteps):            
+            # Select action, transition, update policy
+            a = pi.select_action(s,epsilon)
+            s_next,r,done = env.step(a)
+            total_r += r
+            rewards[t] = total_r
+            pi.update(s, a, r, done, s_next,n_planning_updates=n_planning_updates)
+            
+            # Reset environment when terminated
+            if done:
+                s = env.reset()
+            else:
+                s = s_next
+        cumulative_rewards += rewards
+    cumulative_rewards /= n_repetitions
     
     # Apply additional smoothing
-    learning_curve = smooth(learning_curve,smoothing_window) # additional smoothing
+    learning_curve = smooth(cumulative_rewards,smoothing_window) # additional smoothing
+    
     return learning_curve
 
 
@@ -34,43 +66,45 @@ def experiment():
     smoothing_window = 101
     gamma = 0.99
 
-    for policy in ['Dyna','Prioritized Sweeping']:
+    for policy in ["Dyna","Prioritized Sweeping"]:
     
         ##### Assignment a: effect of epsilon ######
         learning_rate = 0.5
         n_planning_updates = 5
         epsilons = [0.01,0.05,0.1,0.25]
-        Plot = LearningCurvePlot(title = '{}: effect of $\epsilon$-greedy'.format(policy))
+        Plot = LearningCurvePlot(title = "{}: effect of $\epsilon$-greedy".format(policy))
         
         for epsilon in epsilons:
             learning_curve=run_repetitions(policy, n_repetitions, n_timesteps, smoothing_window, 
                                            learning_rate, gamma, epsilon, n_planning_updates)
-            Plot.add_curve(learning_curve,label='$\epsilon$ = {}'.format(epsilon))
-        Plot.save('{}_egreedy.png'.format(policy))
+            Plot.add_curve(learning_curve,label="$\epsilon$ = {}".format(epsilon))
+        Plot.save("{}_egreedy.png".format(policy))
         
         ##### Assignment b: effect of n_planning_updates ######
         epsilon=0.1
         n_planning_updatess = [1,5,15]
         learning_rate = 0.5
-        Plot = LearningCurvePlot(title = '{}: effect of number of planning updates per iteration'.format(policy))
+        Plot = LearningCurvePlot(title = "{}: effect of number of planning updates per iteration".format(policy))
 
         for n_planning_updates in n_planning_updatess:
             learning_curve=run_repetitions(policy, n_repetitions, n_timesteps, smoothing_window, 
                                            learning_rate, gamma, epsilon, n_planning_updates)
-            Plot.add_curve(learning_curve,label='Number of planning updates = {}'.format(n_planning_updates))
-        Plot.save('{}_n_planning_updates.png'.format(policy))  
+            Plot.add_curve(learning_curve,label="Number of planning updates = {}".format(n_planning_updates))
+        Plot.save("{}_n_planning_updates.png".format(policy))  
         
         ##### Assignment 1c: effect of learning_rate ######
         epsilon=0.1
         n_planning_updates = 5
         learning_rates = [0.1,0.5,1.0]
-        Plot = LearningCurvePlot(title = '{}: effect of learning rate'.format(policy))
+        Plot = LearningCurvePlot(title = "{}: effect of learning rate".format(policy))
     
         for learning_rate in learning_rates:
             learning_curve=run_repetitions(policy, n_repetitions, n_timesteps, smoothing_window, 
                                            learning_rate, gamma, epsilon, n_planning_updates)
-            Plot.add_curve(learning_curve,label='Learning rate = {}'.format(learning_rate))
-        Plot.save('{}_learning_rate.png'.format(policy)) 
+            Plot.add_curve(learning_curve,label="Learning rate = {}".format(learning_rate))
+        Plot.save("{}_learning_rate.png".format(policy)) 
     
-if __name__ == '__main__':
+if __name__ == "__main__":
     experiment()
+    print(["123", "456"][0])
+        
